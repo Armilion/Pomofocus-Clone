6ea9117bdf482a5051a83e64bcb9c48c7f40223a
Armilion <valentin.andre94@gmail.com>
2021-09-02 12:44:25 +0200

    Implementation aria-controls between tab and tabpanel

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
 import './App.css';
 
+// MUI
+import { Tab, Tabs } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+
+//SwipeableViews
+import SwipeableViews from 'react-swipeable-views';
+
+// Local imports 
+import Task from "./components/Task";
+import Timer from "./components/Timer";
+import Completion from "./components/Completion";
+import TabPanel from './components/TabPanel';
+import { useState } from 'react';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow:1,
+    backgroundColor: theme.palette.background.paper
+  },
+  tabs : {
+    flex:1,
+    justifyContent:"space-between"
+  },
+  tab: {
+  }
+}))
+
 function App() {
+  const [value, setValue] = useState("0");
+  const classes = useStyles();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Tabs className={classes.tabs} centered onChange={(e, newValue) => setValue(newValue)} aria-label="Pomodoro">
+          <Tab className={classes.tab} label="Task" id='simple-tab-0' aria-controls='tabpanel-0'/>
+          <Tab className={classes.tab} label="Timer" id='simple-tab-1' aria-controls='tabpanel-1' />
+          <Tab className={classes.tab} label="Completion" id='simple-tab-2' aria-controls='tabpanel-2' />
+        </Tabs>
+      </AppBar>
+      <SwipeableViews index={value}>
+        <TabPanel value={value} index={0}>
+          <Task />
+        </TabPanel>
+        <TabPanel value={value} index={1}>
+          <Timer />
+        </TabPanel >
+        <TabPanel value={value} index={2}>
+          <Completion />
+        </TabPanel>
+      </SwipeableViews>
     </div>
   );
 }
