26eec57162aa90e9df34abf20d5dfac363c5f6dc
Armilion <valentin.andre94@gmail.com>
2021-09-03 18:25:02 +0200

    Layout appbar + layout timer

--- src/App.js
+++ src/App.js
@@ -1,58 +1,105 @@
 import './App.css';
 
 // MUI
-import { Tab, Tabs } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
+import { Button, Tab, Tabs, Typography, Paper, Container } from '@material-ui/core';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
-
-//SwipeableViews
-import SwipeableViews from 'react-swipeable-views';
 
 // Local imports 
 import Task from "./components/Task";
 import Timer from "./components/Timer";
 import Completion from "./components/Completion";
 import TabPanel from './components/TabPanel';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import CheckCircleIcon from '@material-ui/icons/CheckCircle';
+import SettingsIcon from '@material-ui/icons/Settings';
 
 const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow:1,
-    backgroundColor: theme.palette.background.paper
-  },
-  tabs : {
-    flex:1,
-    justifyContent:"space-between"
-  },
-  tab: {
-  }
+    root: {
+        display: "flex",
+        justifyContent: "center",
+        height:"100vh",
+        backgroundColor: theme.palette.secondary.main,
+        color: "#fff"
+    },
+    subRoot: {
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+    },
+    appBar: {
+        display: "flex",
+        width: "100%",
+        flexDirection: "row",
+        justifyContent: "space-between",
+        backgroundColor: theme.palette.secondary.main,
+        padding: "5px",
+        borderBottom: "1px solid rgba(0,0,0,0.2)"
+    },
+    button: {
+        backgroundColor: "rgba(255,255,255,0.2)",
+        color: "#fff",
+        fontSize: "13px"
+    },
+    paper: {
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        padding: "20px",
+        margin: "20px",
+        backgroundColor: theme.palette.secondary.light,
+        color: "white"
+    },
+    startButton: {
+        fontSize: "22px",
+        color: theme.palette.secondary.main,
+        backgroundColor: "white",
+        boxShadow: "0px 5px 0px 0px rgb(235,235,235)"
+    }
 }))
 
-function App() {
-  const [value, setValue] = useState("0");
-  const classes = useStyles();
-  return (
-    <div className={classes.root}>
-      <AppBar position="static">
-        <Tabs className={classes.tabs} centered onChange={(e, newValue) => setValue(newValue)} aria-label="Pomodoro">
-          <Tab className={classes.tab} label="Task" id='simple-tab-0' aria-controls='tabpanel-0'/>
-          <Tab className={classes.tab} label="Timer" id='simple-tab-1' aria-controls='tabpanel-1' />
-          <Tab className={classes.tab} label="Completion" id='simple-tab-2' aria-controls='tabpanel-2' />
-        </Tabs>
-      </AppBar>
-      <SwipeableViews index={value}>
-        <TabPanel value={value} index={0}>
-          <Task />
-        </TabPanel>
-        <TabPanel value={value} index={1}>
-          <Timer />
-        </TabPanel >
-        <TabPanel value={value} index={2}>
-          <Completion />
-        </TabPanel>
-      </SwipeableViews>
-    </div>
-  );
+
+function App(props) {
+    const [value, setValue] = useState(0);
+    const classes = useStyles();
+    const theme = useTheme();
+    const handleChange = (e, newValue) => {
+        setValue(newValue)
+    }
+
+    return (
+        <Container maxWidth="xl" className={classes.root}>
+            <Container maxWidth="sm" className={classes.subRoot}>
+                <Container className={classes.appBar}>
+                    <Typography variant="h6"><CheckCircleIcon />Pomofocus</Typography>
+                    <Button className={classes.button} variant="contained" disableElevation startIcon={<SettingsIcon />}>Settings</Button>
+                </Container>
+                <Paper className={classes.paper} elevation={0}>
+                    <Tabs value={value} centered onChange={handleChange} aria-label="Pomodoro tabs" TabIndicatorProps={{ style: { display: "none" } }}>
+                        <Tab label="Pomodoro" id='tab-0' aria-controls='tabpanel-0' />
+                        <Tab label="Short Break" id='tab-1' aria-controls='tabpanel-1' />
+                        <Tab label="Long Break" id='tab-2' aria-controls='tabpanel-2' />
+                    </Tabs>
+                    <Typography variant="h1">25:00</Typography>
+                    <Button variant="contained" className={classes.startButton} disableElevation>Start</Button>
+                </Paper>
+                <Container>
+                    <Typography variant="h6">Tasks</Typography>
+                    
+                </Container>
+            </Container>
+        </Container>
+    );
 }
+
+{/* <TabPanel value={value} index={0}>
+                <Task />
+            </TabPanel>
+            <TabPanel value={value} index={1}>
+                <Timer />
+            </TabPanel >
+            <TabPanel value={value} index={2}>
+                <Completion />
+            </TabPanel> */}
 
 export default App;
